4c9ea543286891c94b6e3bf6e1ac88808992a69b
Andrez-11 <lan_nova1110@hotmail.com>
2014-10-10 00:57:46 -0500

    Carpeta Imagenes y Modificacion a Iniciar Sesion

--- Proyecto_Lipac/Inicio_Sesion.Designer.cs
+++ Proyecto_Lipac/Inicio_Sesion.Designer.cs
@@ -28,92 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Inicio_Sesion));
             this.txt_usuario = new System.Windows.Forms.TextBox();
             this.txt_contra = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.btn_entrar = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.btn_entrar = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // txt_usuario
             // 
-            this.txt_usuario.Location = new System.Drawing.Point(192, 104);
+            this.txt_usuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_usuario.Location = new System.Drawing.Point(145, 92);
             this.txt_usuario.Name = "txt_usuario";
-            this.txt_usuario.Size = new System.Drawing.Size(124, 20);
+            this.txt_usuario.Size = new System.Drawing.Size(162, 26);
             this.txt_usuario.TabIndex = 0;
             // 
             // txt_contra
             // 
-            this.txt_contra.Location = new System.Drawing.Point(192, 143);
+            this.txt_contra.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_contra.Location = new System.Drawing.Point(145, 135);
             this.txt_contra.Name = "txt_contra";
-            this.txt_contra.Size = new System.Drawing.Size(124, 20);
+            this.txt_contra.Size = new System.Drawing.Size(162, 26);
             this.txt_contra.TabIndex = 1;
+            this.txt_contra.UseSystemPasswordChar = true;
             this.txt_contra.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(89, 107);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(63, 98);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.Size = new System.Drawing.Size(76, 16);
             this.label1.TabIndex = 2;
             this.label1.Text = "USUARIO";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(89, 146);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(27, 141);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(91, 13);
+            this.label2.Size = new System.Drawing.Size(112, 16);
             this.label2.TabIndex = 3;
             this.label2.Text = "CONTRASEÑA";
-            // 
-            // btn_entrar
-            // 
-            this.btn_entrar.BackColor = System.Drawing.SystemColors.InactiveCaption;
-            this.btn_entrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_entrar.Location = new System.Drawing.Point(159, 198);
-            this.btn_entrar.Name = "btn_entrar";
-            this.btn_entrar.Size = new System.Drawing.Size(75, 23);
-            this.btn_entrar.TabIndex = 4;
-            this.btn_entrar.Text = "Entrar ";
-            this.btn_entrar.UseVisualStyleBackColor = false;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
-            this.label3.Location = new System.Drawing.Point(127, 9);
+            this.label3.Location = new System.Drawing.Point(106, 39);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(184, 25);
             this.label3.TabIndex = 5;
             this.label3.Text = "INICIAR SESION";
             // 
-            // pictureBox1
+            // btn_entrar
             // 
-            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(-7, -3);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(128, 82);
-            this.pictureBox1.TabIndex = 6;
-            this.pictureBox1.TabStop = false;
+            this.btn_entrar.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btn_entrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_entrar.Location = new System.Drawing.Point(164, 181);
+            this.btn_entrar.Name = "btn_entrar";
+            this.btn_entrar.Size = new System.Drawing.Size(96, 31);
+            this.btn_entrar.TabIndex = 4;
+            this.btn_entrar.Text = "Entrar ";
+            this.btn_entrar.UseVisualStyleBackColor = false;
             // 
             // Inicio_Sesion
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.ClientSize = new System.Drawing.Size(391, 256);
-            this.Controls.Add(this.pictureBox1);
+            this.ClientSize = new System.Drawing.Size(370, 221);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.btn_entrar);
             this.Controls.Add(this.label2);
@@ -123,7 +111,6 @@
             this.Name = "Inicio_Sesion";
             this.Text = "Inicio Sesion";
             this.Load += new System.EventHandler(this.Inicio_Sesion_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -137,6 +124,5 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button btn_entrar;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
